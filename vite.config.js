128e0005de02752862e70405aeb79a3be05a3720
Ajmalshaikh04 <ajshaikh0404@gmail.com>
2023-12-12 07:09:53 +0530

    react advanced topic

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   root: "src",
+  // Set the base path for your project (default is '/')
+  base: "/adoptme-v8/",
+
+  // Output directory for the production build
+  build: {
+    outDir: "dist",
+  },
 });
